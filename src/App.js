8e2a88edf9adae72d536762759585045ff03fc43
kalair22 <kalaivaniravi2208@gmail.com>
2024-07-08 17:56:17 +0530

    Loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
-
+import { useState,useEffect } from 'react';
+import BubbleLoader from './component/BubbleLoader';
 import Home from './pages/Home';
 
 
 function App() {
+ const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Adjust the timeout duration as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      
-        <Home/>
-      
-      
+      {isLoading ? <BubbleLoader /> : <Home />}
     </div>
   );
 }
---
+++ src/component/BubbleLoader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const BubbleLoader = () => {
+  return (
+    <div className="loader">
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+    </div>
+  );
+};
+
+export default BubbleLoader;
